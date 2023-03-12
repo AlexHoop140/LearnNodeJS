3aaeabdd0e1c1728e3a8187e8b4604ec1244e770
AlexHoop140 <alexhoop.pro@gmail.com>
2023-03-13 00:19:57 +0700

    static files and viewEngine

---
+++ src/server.js
@@ -0,0 +1,19 @@
+// const express = require('express')
+import express from 'express';
+import configViewEngine from './config/viewEngine.js';
+require('dotenv').config();
+
+const app = express()
+const path = require('path');
+const port = process.env.PORT || 8080;
+console.log(">>> check port: ", port);
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
